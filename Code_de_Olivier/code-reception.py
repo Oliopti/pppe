a21710aef0ae4dda861ec6328c042968ad90b612
Oliopti <100162580+Oliopti@users.noreply.github.com>
2023-06-04 16:14:24 +0200

    Create code-reception.py

---
+++ Code_de_Olivier/code-reception.py
@@ -0,0 +1,28 @@
+import time
+import serial
+
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyUSB1',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=5
+)
+
+if ser.isOpen():
+    ser.close()
+ser.open()
+ser.isOpen()
+
+
+while True:
+    try :
+        res=ser.read(6)
+        print(res)
+        time.sleep(1)
+    except:
+        print('erreur')
+        
