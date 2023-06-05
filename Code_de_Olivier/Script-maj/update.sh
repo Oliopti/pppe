608a06ff20e19b744bad6458bb487433ddbc9730
Oliopti <olivier.jourdaintechnicien@gmail.com>
2023-06-05 17:30:40 +0200

    maj code update.sh

--- Code_de_Olivier/Script-maj/update.sh
+++ Code_de_Olivier/Script-maj/update.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Code rédiger par Olivier JOURDAIN le 10/04/2023
+# Code rédigé par Olivier JOURDAIN le 10/04/2023
 
-
-echo "Exécution de la commande pour télécharger les paquets de mise à jours :"
+echo -e "\033[1;32mExécution de la commande pour télécharger les paquets de mise à jour :\033[0m"
 sudo apt-get update -y && sudo apt update -y
 
-echo "Exécution de la commande pour lister tous les paquets à mettre a jour :"
+echo -e "\033[1;32mExécution de la commande pour lister tous les paquets à mettre à jour :\033[0m"
 apt list --upgradable && apt list --upgradable -a
 
-echo "Exécution de la commande pour mettre a jours tous les paquets :"
+echo -e "\033[1;32mExécution de la commande pour mettre à jour tous les paquets :\033[0m"
 sudo apt-get upgrade -y && sudo apt upgrade -y 
 
-echo "Exécution de la commande pour supprimer les paquets inutiles :"
+echo -e "\033[1;32mExécution de la commande pour supprimer les paquets inutiles :\033[0m"
 sudo apt-get autoremove -y && sudo apt autoremove -y
 
-
-echo "Mise à jour terminée."
-
-
+echo -e "\033[1;36mMise à jour terminée.\033[0m"
 
 
