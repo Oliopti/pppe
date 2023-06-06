e70c36b166d46c14c2eacba34e0c50874a25368e
Oliopti <olivier.jourdaintechnicien@gmail.com>
2023-06-06 11:15:57 +0200

    Update main.py

--- Code_de_Olivier/reception-envoie-serveur/main.py
+++ Code_de_Olivier/reception-envoie-serveur/main.py
@@ -3,6 +3,45 @@ import serial
 
 import mysql.connector
 
+def insertion(mesures):
+    try:
+        # Connection au serveur MariaDB
+        connection = mysql.connector.connect(
+            #host = '192.168.0.108',
+            host='172.20.10.26',
+            database='pppe',
+            user='admin',
+            password='admin'
+        )
+
+        print("Essai de connexion au serveur MySQL")
+
+        # Création d'un curseur pour exécuter des requêtes SQL
+        cursor = connection.cursor()
+
+        # Construction de la requête d'insertion avec la valeur fournie
+        mySql_insert_query = f"INSERT INTO releve_puissance(id_session, mesures) VALUES((SELECT MAX(id) FROM session), {mesures})"
+
+        print(mySql_insert_query)
+
+        # Exécution de la requête d'insertion
+        cursor.execute(mySql_insert_query)
+
+        # Validation des modifications dans la base de données
+        connection.commit()
+
+        # Affichage de la requête d'insertion
+        print("Exécuter la commande :", mySql_insert_query)
+
+        # Fermeture du curseur
+        cursor.close()
+
+        print("Enregistrement inséré avec succès dans la table releve_puissance")
+    except mysql.connector.Error as error:
+        print("Échec de l'insertion d'un enregistrement dans la table :", error)
+        return False
+    return
+
 # Configuration de la connexion série
 ser = serial.Serial(
     port='/dev/ttyUSB0',                  # Port série à utiliser
@@ -30,57 +69,22 @@ while True:
         res = ser.read(6)
 
         # Décodage des données lues en une chaîne de caractères lisible
-        print(res.decode())
+        res=res.decode()
+        res=res.split("-")
+        print("Signal recu :",res)
+        # Appel de la fonction insertion avec la valeur 321
+        insertion(res[1])
+
 
         # Attente de 1 seconde avant la prochaine lecture
-        time.sleep(5)
+        time.sleep(1)
 
     # Gestion des exceptions en cas d'erreur
     except:
-        print('erreur')
+        print('erreur while true')
 
 
 
 
-def insertion(mesures):
-    try:
-        # Connection au serveur MariaDB
-        connection = mysql.connector.connect(
-            #host = '192.168.0.108',
-            host='172.20.10.26',
-            database='pppe',
-            user='admin',
-            password='admin'
-        )
 
-        print("Essai de connexion au serveur MySQL")
-
-        # Création d'un curseur pour exécuter des requêtes SQL
-        cursor = connection.cursor()
-
-        # Construction de la requête d'insertion avec la valeur fournie
-        mySql_insert_query = "INSERT INTO releve_puissance(id_session, mesures) VALUES((SELECT MAX(id) FROM session), 0)"
-
-        print(mySql_insert_query)
-
-        # Exécution de la requête d'insertion
-        cursor.execute(mySql_insert_query)
-
-        # Validation des modifications dans la base de données
-        connection.commit()
-
-        # Affichage de la requête d'insertion
-        print("Exécuter la commande :", mySql_insert_query)
-
-        # Fermeture du curseur
-        cursor.close()
-
-        print("Enregistrement inséré avec succès dans la table releve_puissance")
-    except mysql.connector.Error as error:
-        print("Échec de l'insertion d'un enregistrement dans la table :", error)
-        return False
-    return
-
-# Appel de la fonction insertion avec la valeur 321
-insertion(res)
 
